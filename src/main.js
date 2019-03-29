af0845796482ff5cc5563943535d73be86a483de
liyicky <liyicky@gmail.com>
2019-03-29 12:27:16 +0900

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+/* Install Vue */
 import Vue from 'vue'
 import App from './App.vue'
 
+/* Install Bootstrap */
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+/* Setup Vue */
 Vue.config.productionTip = false
 
 new Vue({
